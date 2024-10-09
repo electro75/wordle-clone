dd1e96247c3b34d0f81fd55d4839e9ca522c89e7
Sahil Naikdhure <sanaikdhure@gmail.com>
2024-10-09 17:11:17 +0100

    add guess component

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,34 +1,23 @@
 import React from 'react';
+import Guess from '../Guess';
 
 function GuessResults({ guessList }) {
   const rows = new Array(5).fill('');
   const cols = new Array(6).fill(rows);
 
-  const getRow = (guessNumber) => {
-    const emptyRow = rows.map(() => <span className="cell" key={crypto.randomUUID()} ></span>) 
-    if(!guessList.length) return emptyRow
-
+  const getGuess = (guessNumber) => {
+    if(!guessList.length) return <Guess guess=''/>
     if(guessNumber < guessList.length) {
-      const guess = guessList[guessNumber];
-      return (
-          guess.split('').map((char, idx) => {
-            return <span className="cell" key={idx}> {char} </span>
-          }
-        )
-      )
+      return <Guess guess={guessList[guessNumber]} />
     }
-
-    return emptyRow
+    return <Guess guess=''/>
   }
 
   return (
     <div className="guess-results">
-      {/* {guessList.map((guess) => (
-        <p className="guess" key={crypto.randomUUID()}>{guess}</p>
-      ))} */}
       {cols.map((row, index) => (
         <p className="guess" key={crypto.randomUUID()}>
-          {getRow(index)}
+          {getGuess(index)}
         </p>
       ))}
     </div>
