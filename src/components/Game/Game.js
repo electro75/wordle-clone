e3428f0bdfdea02ff0054e3c08c46178f1ae2043
Sahil Naikdhure <30566861+electro75@users.noreply.github.com>
2024-10-09 17:12:28 +0100

    Revert "Add Guess Input component"

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-import GuessInput from '../GuessInput';
-import GuessResults from '../GuessResults';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -11,15 +9,7 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-
-  const [guessList, setGuessList] = useState([]);
-
-  return (
-    <>
-      <GuessResults guessList={guessList}/>
-      <GuessInput guessList={guessList} setGuessList={setGuessList} />
-    </>
-  );
+  return <>Put a game here!</>;
 }
 
 export default Game;
